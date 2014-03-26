1af8c8623c0580b926fc9f6f8b7363673a3cb668
neuroman <neuroman@users.noreply.github.com>
2014-03-26 12:53:21 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'WTRvuTjKIJ4ok0Ht34WVMrs52lUMO1MHBliyQS6uGSOegn8K', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1737284512], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Neuroman Test Page, // Replaces Xively logo in the header
+		applicationName	= 'Neuroman Test Page', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
 		dataColor		= 'AABBCC', // CSS HEX value of color to represent data (omit leading #)
